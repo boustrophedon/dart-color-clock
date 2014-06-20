04ac7a40112afd8242e4c2d70f4b3d0eda97fee4
Harry Stern <hcs@synonymbun.com>
2014-06-19 22:58:22 -0400

    haha not the only commit because I realized I should have just put the hex string i was already computing into the background property.

--- web/main.dart
+++ web/main.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 
 void main() {
   var time = new DivElement();
-  time.text = "test";
   document.body.children.add(time);
 
   var timer = new Timer.periodic(const Duration(seconds: 1), (t)=>update_time(time));
@@ -13,9 +12,9 @@ void main() {
 void update_time(HtmlElement e) {
   var d = new DateTime.now();
   num r = d.hour, g = d.minute, b = d.second;
-  document.body.style.background = 'rgb($r, $g, $b)';
   var rs = r.toString().padLeft(2, '0');
   var gs = g.toString().padLeft(2, '0');
   var bs = b.toString().padLeft(2, '0');
   e.text = "#$rs$gs$bs";
+  document.body.style.background = '#$rs$gs$bs';
 }
